ce053f76ae25ca65569644f98d39245e61b8b05d
ne_srv <ne_srv@mail.ru>
2025-04-16 11:07:47 +0300

    day-2

--- myShop/myShop/settings.py
+++ myShop/myShop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+
+CART_SESSION_ID = 'cart'
--- myShop/shop/views.py
+++ myShop/shop/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+from cart.forms import CartAddProductForm
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'shop/product/list.html', {'products': products})
+    return render(request, "shop/product/list.html", {"products": products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, "shop/product/detail.html", {"product": product})
+
+
+# ...
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id,
+    slug=slug,
+    available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+    'shop/product/detail.html',
+    {'product': product,
+    'cart_product_form': cart_product_form})
